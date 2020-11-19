eb7cce38bd5898f5162afd9f750e3d37a5bd8b55
git-shashwat <shashwat824@gmail.com>
2020-11-19 23:43:29 +0530

    added flatlist for goal lists

--- App.js
+++ App.js
@@ -1,31 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
   return (
-    <View style={{ padding: 50 }}>
+    <View style={styles.screen}>
       <View 
-        style={{ 
-          flexDirection: 'row', 
-          justifyContent: 'space-between', 
-          alignItems: 'center' 
-        }}>
+        style={styles.inputContainer}>
         <TextInput 
           placeholder="Course Goal" 
-          style={{ 
-            borderBottomColor: 'black', 
-            borderBottomWidth: 1, 
-            marginBottom: 10,
-            width: '80%'
-          }} 
+          style={styles.input} 
+          value={task}
+          onChangeText={text => { setTask(text); }}
         />
-        <Button title="ADD" />
+        <View style={styles.button}>
+          <Button title="ADD" onPress={() => {
+            if (task != '') {
+              setTaskList([...taskList, task]);
+              setTask('');
+              }
+            }
+          } />        
+        </View>
+        <View>
+          <Button style={styles.button} color="red" title="X" onPress={() => {
+            setTaskList([]);
+            setTask('');
+            }
+          } />
+        </View>
       </View>
-      <View>
-      
-      </View>
+        {
+          taskList.length === 0 ? (
+            <Text>All goals achieved 😄</Text>
+          ) :
+          <FlatList 
+            data={taskList} 
+            renderItem={itemData => <View style={styles.listItem}><Text>{itemData.item}</Text></View>} 
+            keyExtractor={itemData => itemData.index} 
+          />
+        }
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: { 
+    padding: 50
+  },
+  inputContainer: { 
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center' 
+  },
+  input: { 
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1, 
+    marginBottom: 20,
+    width: '75%'
+  },
+  listItem: {
+    alignItems: 'center',
+    backgroundColor: '#DA746F',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
+  },
+  button: {
+    marginRight: 2
+  }
+});
